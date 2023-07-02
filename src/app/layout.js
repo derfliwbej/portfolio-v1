9370e6c8f4cb820d12aa88234d25d16473959fd5
derfliwbej <jwdpanganiban@gmail.com>
2023-07-02 12:54:39 +0800

    Initial commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,10 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Ubuntu_Mono } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const ubuntu_mono = Ubuntu_Mono({ 
+  subsets: ['latin'],
+  weight: '400'
+});
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={ubuntu_mono.className}>{children}</body>
     </html>
   )
 }
