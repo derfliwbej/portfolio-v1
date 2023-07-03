ecb5dab6a858351440b1a529cddcdfe53f03109f
derfliwbej <jwdpanganiban@gmail.com>
2023-07-03 20:18:18 +0800

    Feat: Added skills section

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Nav from "@/components/nav";
 import Hero from "@/components/hero";
 import Links from "@/components/links";
+import Skills from "@/components/skills";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Nav />
       <div className="px-10 md:px-28 lg:px-28">
         <Hero />
+        <Skills />
       </div>
       <Links />
     </main>
