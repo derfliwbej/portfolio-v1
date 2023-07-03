59f06155a9670923699e918fb6144d0f70c389da
derfliwbej <jwdpanganiban@gmail.com>
2023-07-03 13:38:33 +0800

    Feat: Added Links component

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Nav from "@/components/nav";
 import Hero from "@/components/hero";
+import Links from "@/components/links";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <div className="px-10 md:px-28 lg:px-28">
         <Hero />
       </div>
+      <Links />
     </main>
   );
 }
