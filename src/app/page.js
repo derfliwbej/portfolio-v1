160673ee79a57a1f878f74ef85ae913dc5c9bdbd
derfliwbej <jwdpanganiban@gmail.com>
2023-07-06 01:56:18 +0800

    Feat: Add experiences section

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,16 @@ import Nav from "@/components/nav";
 import Hero from "@/components/hero";
 import Links from "@/components/links";
 import Skills from "@/components/skills";
+import Experiences from "@/components/experiences";
 
 export default function Home() {
   return (
     <main>
       <Nav />
-      <div className="px-10 md:px-28 lg:px-28">
+      <div className="px-3 md:px-28 lg:px-28">
         <Hero />
         <Skills />
+        <Experiences />
       </div>
       <Links />
     </main>
