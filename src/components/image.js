6375a764edf514a575461690ed1bf757d75e9321
derfliwbej <jwdpanganiban@gmail.com>
2023-07-06 02:16:24 +0800

    Feat: Add callback for handling image load complete

--- src/components/image.js
+++ src/components/image.js
@@ -4,15 +4,16 @@ import { useState } from 'react';
 
 import styles from '@/styles/Hero.module.css'
 
-const BootstrapImage = ({ className, src, alt, size }) => {
+const BootstrapImage = ({ className, src, alt, size, handleLoadComplete }) => {
     const [ratio, setRatio] = useState(16 / 9);
     return (
         <>
             <Image className={className} src={src} alt={alt} width={size} height={size / ratio} fixed="true" onLoadingComplete={({ naturalWidth, naturalHeight }) => {
+                if (handleLoadComplete) handleLoadComplete();
                 setRatio(naturalWidth / naturalHeight);
             }} />
         </>
     );
 };
 
-export default BootstrapImage
+export default BootstrapImage;
