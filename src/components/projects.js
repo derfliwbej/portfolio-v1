7ed4bf9dc8113dbdedf7b63422df4d603209b438
derfliwbej <jwdpanganiban@gmail.com>
2023-07-09 13:45:58 +0800

    Feat: Added React as a tool

--- src/components/projects.js
+++ src/components/projects.js
@@ -4,7 +4,7 @@ import { Project, ProjectImages, ProjectContent,
 
 import slideshowStyles from '@/styles/projects/cemetery_system/Slideshow.module.css';
 
-export default function Projectss() {
+export default function Projects() {
     return (
         <section id="projects" className="py-5 max-w-5xl mx-auto">
             <h1 className="mb-10 text-center text-xl text-lime-400 font-bold md:text-3xl">Some Projects I Built</h1>
@@ -26,7 +26,7 @@ export default function Projectss() {
                         <span><a href="#" aria-label="Open Demo"><svg className="fill-white hover:fill-blue-400" xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 512 512"><path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"/></svg></a></span>
                     </ProjectLinks>
                     <ProjectDescription description="A web-based cemetery mapping information system for Eternal Place Memorial Park. Administrators can read, add, update, and delete cemetery information. Lot owners can view their owned lots, and make transactions." 
-                    tools={["NextJS", "Supabase", "MaterialUI"]}
+                    tools={["Next", "React", "Supabase", "MaterialUI"]}
                     />
                 </ProjectContent>
             </Project>
