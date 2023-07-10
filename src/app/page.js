85335428de528cd1b96174d3f0ab90876042632c
derfliwbej <jwdpanganiban@gmail.com>
2023-07-10 11:18:28 +0800

    Feat: Add contact section and footer

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,14 @@ import Links from "@/components/links";
 import Skills from "@/components/skills";
 import Experiences from "@/components/experiences";
 import Projects from "@/components/projects";
+import Contact from "@/components/contact";
+
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({ 
+  subsets: ['latin'],
+  weight: '500'
+});
 
 export default function Home() {
   return (
@@ -14,8 +22,12 @@ export default function Home() {
         <Skills />
         <Experiences />
         <Projects />
+        <Contact />
       </div>
       <Links />
+      <div className="text-center text-sm py-5">
+        <p className={poppins.className}>Built by Jeb Panganiban 2023</p>
+      </div>
     </main>
   );
 }
