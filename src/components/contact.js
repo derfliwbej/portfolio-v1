8a483b1ccd4cedd076c7ba8dacffdf79b87f33ec
derfliwbej <jwdpanganiban@gmail.com>
2023-07-10 11:18:11 +0800

    Feat: Create contact section

---
+++ src/components/contact.js
@@ -0,0 +1,10 @@
+export default function Contact() {
+    return (
+        <section id="contact" className="max-w-xl text-center mx-auto pt-5 pb-20">
+            <h2 className="mb-10 text-center text-3xl text-lime-400 font-bold md:text-5xl">Contact Me</h2>
+            <p className="md:text-lg">
+                If you have any questions or just want to say hi, I'd love to hear from you. You can contact me through my email at <span className="text-blue-400 hover:font-bold"><a href="mailto:jwdpanganiban@gmail.com">jwdpanganiban@gmail.com</a></span>, and I'll try to get back to you as soon as I can.
+            </p>
+        </section>
+    );
+}
