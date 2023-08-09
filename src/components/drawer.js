2e97dc6ad8f200a74351c64eb43883b2ae6eb0ad
derfliwbej <jwdpanganiban@gmail.com>
2023-08-10 00:21:53 +0800

    Feat: Make drawer take full width on smaller screens

--- src/components/drawer.js
+++ src/components/drawer.js
@@ -10,7 +10,7 @@ export default function Drawer() {
 
     return (
         <>
-            <aside className="w-[300px] px-12 py-5 bg-[#2C001E] z-30 fixed right-0 top-0 bottom-0 h-screen shadow-lg animate-drawer-open">
+            <aside className="w-[min(100vw,_300px)] px-12 py-5 bg-[#2C001E] z-30 fixed right-0 top-0 bottom-0 h-full shadow-lg animate-drawer-open">
                 <div className="flex justify-end">
                     <div className="cursor-pointer" onClick={closeDrawer}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" height="1.25em" viewBox="0 0 384 512"><path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/></svg>
